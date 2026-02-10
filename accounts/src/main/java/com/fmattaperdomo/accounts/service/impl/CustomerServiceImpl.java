217fafc1097292ae29a9ebb99a4eb78b1f9ed1dc
fmattaperdomo <fmattaperdomo@gmail.com>
2026-02-10 17:08:31 -0500

    ✨:adjustment in search methods

--- accounts/src/main/java/com/fmattaperdomo/accounts/service/impl/CustomerServiceImpl.java
+++ accounts/src/main/java/com/fmattaperdomo/accounts/service/impl/CustomerServiceImpl.java
@@ -71,9 +71,9 @@ public class CustomerServiceImpl implements CustomerService {
     @Override
     public List<CustomerResponseDto> getCustomers() {
         List<Customer> customers = customerRepository.findAll();
-        if(customers.isEmpty()) {
-            throw new ResourceNotFoundException("Customer","customer","All");
-        }
+        //if(customers.isEmpty()) {
+        //    throw new ResourceNotFoundException("Customer","customer","All");
+        //}
 
         return customers.stream()
                 .map(customer -> {
